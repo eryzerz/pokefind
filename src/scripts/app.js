c880e67d7c4e1ee4867176aa05c1eef48d7f7d8b
eryzerz <eryzero@gmail.com>
2020-06-14 20:43:05 +0700

    Adjust branch content

--- src/scripts/app.js
+++
@@ -1,8 +0,0 @@
-import 'regenerator-runtime'
-import 'nouislider/distribute/nouislider.css';
-import '../styles/style.css'
-import './components/filter-form.js'
-import './components/adv-filter.js'
-import main from './view/main.js'
-
-document.addEventListener("DOMContentLoaded", main);
